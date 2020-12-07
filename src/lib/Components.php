ac32d065e55fcc4bfe08b7aa834b6753deaa106d
lgdz <>
2020-12-07 17:34:15 +0800

    修复Components类registers为空时的异常

--- src/lib/Components.php
+++ src/lib/Components.php
@@ -41,24 +41,19 @@ class Components extends InstanceClass implements InstanceInterface
         if (empty($methods)) {
             return ['components' => []];
         }
-        foreach ($methods as $key => $value) {
-            if (is_string($key)) {
-                $op   = $key;
-                $args = is_array($value) ? $value : [$value];
-            } else {
-                $op   = $value;
-                $args = [];
-            }
+        foreach ($methods as $op => $args) {
+            $args = is_null($args) ? [] : (is_array($args) ? $args : [$args]);
             if (!$op) {
                 continue;
             } else {
                 try {
+                    print_r($this->registers);
                     if (empty($this->registers) || ($this->registers[0] !== '*' && !in_array($op, $this->registers)))
                         throw new \Exception('未在Registers中注册');
                     elseif (is_null($this->entity))
                         throw new \Exception('未初始化Entity');
                     else
-                        $components[$op] = call_user_func([$this->entity, $op], ...$args);
+                        $components[$op] = $this->entity->$op(...$args);
                 } catch (\Throwable $e) {
                     $components[$op] = $e->getMessage();
                 }
