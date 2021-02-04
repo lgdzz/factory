ecc6c8bdd10833659ce7c6519bcc414651a652d7
lgdz <>
2021-02-04 09:36:28 +0800

    修复Password操作类BUG

--- src/lib/Password.php
+++ src/lib/Password.php
@@ -13,7 +13,7 @@ class Password extends InstanceClass implements InstanceInterface
 
     public function check(string $input_password, string $salt, string $right_password): bool
     {
-        return $$right_password === $this->encode($input_password, $salt);
+        return $right_password === $this->encode($input_password, $salt);
     }
 
     private function encode(string $password, string $salt): string
