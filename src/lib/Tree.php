2278740a496fc9dfdeff5ae8a37f11b9b9366bd8
lgdz <xiongwq@foxmail.com>
2023-09-26 14:59:12 +0800

    -

--- src/lib/Tree.php
+++ src/lib/Tree.php
@@ -4,20 +4,36 @@ namespace lgdz\lib;
 
 class Tree
 {
-    public function build(array $list, $id, string $pid = 'pid')
+    public function build(array $list, $id, string $pid = 'pid', &$already = [])
     {
         $tmp = [];
         foreach ($list as $row) {
             if ($row[$pid] !== $id) continue;
-            $children = $this->build($list, $row['id'], $pid);
+
+            $already[] = $id;
+
+            $children = $this->build($list, $row['id'], $pid, $already);
             if ($children) {
                 $row['has_child'] = true;
-                $row['children']  = $children;
+                $row['children'] = $children;
             } else {
                 $row['has_child'] = false;
             }
             $tmp[] = $row;
         }
         return $tmp;
+    }
+
+    public function builds(array $list, $ids, string $pid = 'pid')
+    {
+        $already = [];
+        $result = [];
+        foreach ($ids as $id) {
+            if (in_array($id, $already)) {
+                continue;
+            }
+            $result[] = $this->build($list, $id, $pid, $already);
+        }
+        return array_merge(...$result);
     }
 }
